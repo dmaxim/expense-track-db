70bbd1ea72f16bb22adc213c94bc8df18651c6a1
Dan Maxim <dmaxim@mxinformatics.com>
2020-01-12 15:55:30 -0700

    update docker compose

--- ExpenseTrack.Database/docker/generate-local-image.sh
+++ ExpenseTrack.Database/docker/generate-local-image.sh
@@ -1,23 +1,25 @@
-﻿$SA_PASSWORD = "aXygpR312feffflxgh"
-$APP_USER_PASSWORD = "somepass#xYGl"
+#!/bin/bash
 
-Write-Host "Building Image"
+SA_PASSWORD="aXygpR312feffflxgh"
+APP_USER_PASSWORD="somepass#xYGl"
+
+echo "Building Image"
 
 docker build -t mxinfo.expensetrack.dbtemp:latest .
 
-Write-Host "Running Image"
+echo "Running Image"
 
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SA_PASSWORD" -p 1934:1433 --name mxinfo.expensetrack.temp -d mxinfo.expensetrack.dbtemp:latest
 
-Write-Host "Initializing Database"
+echo "Initializing Database"
 
 docker exec mxinfo.expensetrack.temp sh -c "/opt/scripts/initialize.sh '$APP_USER_PASSWORD'"
 
-Write-Host "Generating Final Image"
+echo "Generating Final Image"
 
 docker commit mxinfo.expensetrack.temp dmaxim/mxinfo.expensetrack.database:latest
 
-Write-Host "Tear down intermediate containers"
+echo "Tear down intermediate containers"
 
 docker stop mxinfo.expensetrack.temp
 
